7c890bb0c59fcef2edbe3331fcb547c3342c20cd
Steve Johnson <steve@steveasleep.com>
2019-11-03 12:17:04 -0800

    Hello Spacewords

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="increment">
+    Count is: {{ count }}, double is {{ double }}, click to increment.
+  </button>
+</template>
+
+<script>
+import { ref, computed, onMounted } from '@vue/composition-api'
+
+export default {
+  setup() {
+    const count = ref(0)
+    const double = computed(() => count.value * 2)
+
+    function increment() {
+      count.value++
+    }
+
+    onMounted(() => console.log('component mounted!'))
+
+    return {
+      count,
+      double,
+      increment
+    }
+  }
+}
+</script>
