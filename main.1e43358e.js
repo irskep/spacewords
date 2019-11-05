60ec3e27a61eb862889d6a5c01c50e9897aeebf0
Steve Johnson <steve@steveasleep.com>
2019-11-05 14:04:49 -0800

    Updates

--- main.1e43358e.js
+++ main.1e43358e.js
@@ -33151,7 +33151,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59967" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61046" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
