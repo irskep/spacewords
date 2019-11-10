266e556f8317e408aea89eed7afbe1ac80412522
Steve Johnson <steve@steveasleep.com>
2019-11-10 13:22:09 -0800

    Updates

--- main.1e43358e.js
+++ main.1e43358e.js
@@ -32452,7 +32452,7 @@ module.exports = {
     bind: true,
     groups: [{
       tags: [],
-      phrases: ["tentacles", "claws"]
+      phrases: ["tentacles", "claws", "pincers"]
     }, {
       tags: [["habitat", "ocean"]],
       phrases: ["fins", "minutely articulated fins"]
@@ -33827,7 +33827,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60508" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49304" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
